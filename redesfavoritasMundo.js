7b2bc760ab062a8a11dda7e5e33f46aa74247f32
paolamaciel03 <167144196+paolamaciel03@users.noreply.github.com>
2024-11-19 18:58:28 -0300

    Add files via upload

---
+++ redesfavoritasMundo.js
@@ -0,0 +1,226 @@
+console.log('deu certo');
+async function redesFavoritasMundo() {
+
+}
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+}
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+redesFavoritasMundo()
+{Instagram: 16.5, WhatsApp: 16.1, Facebook: 12.8, WeChat: 12.8, TikTok: 7.4, _}
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+}
+
+redesFavoritasMundo()
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+           type: 'pie',            textinfo: 'label+percent'        }    ]}redesFavoritasMundo()
+            const layout = {
+                plot_bgcolor: getCSS('--bg-color'),
+                paper_bgcolor: getCSS('--bg-color'),
+                title: {
+                    text: 'Redes sociais com mais usuários no mundo',
+                    x: 0,
+                    font: {
+                        color: getCSS('--primary-color'),
+                        family: getCSS('--font'),
+                        size: 30
+                    }
+                },
+                xaxis: {
+                    tickfont: tickConfig,
+                    title: {
+                        text: 'nome das redes sociais',
+                        font: {
+                            color: getCSS('--secondary-color')
+                        }
+                    }
+                },
+                yaxis: {
+                    tickfont: tickConfig,
+              import { getCSS } from "./common.js"
+              
+              const layout = {
+                  plot_bgcolor: getCSS('--bg-color'),
+                  paper_bgcolor: getCSS('--bg-color'),
+                  title: {
+                      text: 'Redes sociais que os usuários mais gostam',
+                      x: 0,
+                      font: {
+                          color: getCSS('--primary-color'),
+                          family: getCSS('--font'),
+                          size: 30
+                      }
+                      const layout = {
+                          plot_bgcolor: getCSS('--bg-color'),
+                          paper_bgcolor: getCSS('--bg-color'),
+                          title: {
+                              text: 'Redes sociais que os usuários mais gostam',
+                              x: 0,
+                              font: {
+                                  color: getCSS('--primary-color'),
+                                  family: getCSS('--font'),
+                                  size: 30
+                              }
+                               },
+                                  legend: {
+                                      font: {
+                                          color: getCSS('--primary-color'),
+                                          size: 16
+                                      }
+                                  }
+                                const grafico = document.createElement('div')
+                                grafico.className = 'grafico'
+                                document.getElementById('graficos-container').appendChild(grafico)
+                               Plotly.newPlot(grafico, data, layout)
+              import { getCSS } from "./common.js"
+              
+              async function redesFavoritasMundo() {
+                  const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+                  const res = await fetch(url)
+                  const dados = await res.json()
+                  const redes = Object.keys(dados)
+                  const valores = Object.values(dados)
+              
+                  const data = [
+                      {
+
+values: valores,            labels: redes,            type: 'pie',            textinfo: 'label+percent'        }    ]    const layout = {        plot_bgcolor: getCSS('--bg-color'),        paper_bgcolor: getCSS('--bg-color'),        title: {            text: 'Redes sociais que os usu%C3%A1rios mais gostam',            x: 0,            font: {
+    color: getCSS('--primary-color'),                family: getCSS('--font'),                size: 30            }        },        legend: {            font: {                color: getCSS('--primary-color'),                size: 16
+          const grafico = document.createElement('div')
+            grafico.className = 'grafico'
+            document.getElementById('graficos-container').appendChild(grafico)
+            Plotly.newPlot(grafico, data, layout)
+        }
+        
+        redesFavoritasMundo()
+        {
+          "Instagram": 16.5,
+          "WhatsApp": 16.1,
+          "Facebook": 12.8,
+          "WeChat": 12.8,
+          "TikTok": 7.4,
+          "DouYin": 6.6,
+          "Twitter": 3.2,
+          "Telegram": 2.4,
+          "FB Messenger": 2.3,
+          "LINE": 1.7
+        }
+        console.log('deu certo');
+        async function redesFavoritasMundo() {
+        
+        }
+        async function redesFavoritasMundo() {
+            const url = 'https://raw.githubusercontent.com/paolamacielmacedo/api/main/redes-favoritas.json'
+        }sync function redesFavoritasMundo() {
+            const url = 'https://raw.githubusercontent.com/paolammacedo/api/main/redes-favoritas.json'
+            const res = await fetch(url)
+            const dados = await res.json()
+            console.log(dados);
+        }
+        
+        redesFavoritasMundo()async function redesFavoritasMundo() {
+            const url = 'https://raw.githubusercontent.com/paola/api/main/redes-favoritas.json'
+            const res = await fetch(url)
+            const dados = await res.json()
+            const redes = Object.keys(dados)
+            const valores = Object.values(dados)
+        }
+        
+        redesFavoritasMundo()
+        async function redesFavoritasMundo() {
+            const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+            const res = await fetch(url)
+            const dados = await res.json()
+            const redes = Object.keys(dados)
+            const valores = Object.values(dados)
+        
+            const data = [
+                values: valores,            labels: redes,            type: 'pie',            textinfo: 'label+percent'        }    ]}redesFavoritasMundo()
+                const layout = {
+                    plot_bgcolor: getCSS('--bg-color'),
+                    paper_bgcolor: getCSS('--bg-color'),
+                    title: {
+                        text: 'Redes sociais com mais usuários no mundo',
+                        x: 0,
+                        font: {
+                            color: getCSS('--primary-color'),
+                            family: getCSS('--font'),
+                            size: 30
+                             },
+                                xaxis: {
+                                    tickfont: tickConfig,
+                                    title: {
+                                        text: 'nome das redes sociais',
+                                        font: {
+                                            color: getCSS('--secondary-color')
+                                        }
+                                    }
+                                },
+                                yaxis: {
+                                    tickfont: tickConfig,
+                                    title: {
+                                        text: 'bilhões de usuários ativos',
+                                        font: {
+                                            color: getCSS('--secondary-color
+                                                import { getCSS } from "./common.js"
+                                                const layout = {
+                                                    plot_bgcolor: getCSS('--bg-color'),
+                                                    paper_bgcolor: getCSS('--bg-color'),
+                                                    title: {
+                                                        text: 'Redes sociais que os usuários mais gostam',
+                                                        x: 0,
+                                                        font: {
+                                                            color: getCSS('--primary-color'),
+                                                            family: getCSS('--font'),
+                                                            size: 30
+                                                            const layout = {
+                                                                plot_bgcolor: getCSS('--bg-color'),
+                                                                paper_bgcolor: getCSS('--bg-color'),
+                                                                title: {
+                                                                    text: 'Redes sociais que os usuários mais gostam',
+                                                                    x: 0,
+                                                                    font: {
+                                                                        color: getCSS('--primary-color'),
+                                                                        family: getCSS('--font'),
+                                                                        size: 30
+                                                                    }
+                    const grafico = document.createElement('div')
+                    grafico.className = 'grafico'
+                    document.getElementById('graficos-container').appendChild(grafico)
+                    Plotly.newPlot(grafico, data, layout)
+                                                                    import { getCSS } from "./common.js"
+                       s                                             
+                                                                    async function redesFavoritasMundo() {
+                                                                        const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+                                                                        const res = await fetch(url)
+                                                                        const dados = await res.json()
+                                                                        const redes = Object.keys(dados)
+                                                                        const valores = Object.values(dados)
+                                                                    
+                                                                        const grafico = document.createElement('div')
+                                                                            grafico.className = 'grafico'
+                                                                            document.getElementById('graficos-container').appendChild(grafico)
+                                                                            Plotly.newPlot(grafico, data, layout)
+                                                                        }
+                                  s                                      
---
+++ redessociaMinhaEscola.js
@@ -0,0 +1,59 @@
+console.log('https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF');
+async function redesSociaisFavoritasMinhaEscola() {
+
+}
+async function redesSociaisFavoritasMinhaEscola() {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch(url) 
+    const dados = await res.json()
+
+    console.log(dados)
+}
+
+redesSociaisFavoritasMinhaEscola()
+console.table(dados)
+async function redesSociaisFavoritasMinhaEscola() {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch(url) 
+    const dados = await res.json()
+
+    const redesSociais = dados.slice(1).map(redes = redes[1])
+    console.table(dados)
+}
+
+redesSociaisasync function redesSociaisFavoritasMinhaEscola() {
+
+}
+FavoritasMinhaEscola()
+
+async function redesSociaisFavoritasMinhaEscola() {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch(url) 
+    const dados = await res.json()
+
+    console.log(dados)
+}
+
+redesSociaisFavoritasMinhaEscola()
+console.table(dados)
+async function redesSociaisFavoritasMinhaEscola() {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch(url) 
+    const dados = await res.json()
+
+    const redesSociais = dados.slice(1).map(redes = redes[1])
+    console.table(dados)
+}
+
+redesSociaisFavoritasMinhaEscola()
+async function redesSociaisFavoritasMinhaEscola() {
+    //código omitido
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redesSociais = dados.slice(l).map(redes => redes [1])
+    
+    const contagemRedesSociais = redesSociais.reduce()
+}
+
+redesSociaisFavoritasMinhaEscola()
+s
